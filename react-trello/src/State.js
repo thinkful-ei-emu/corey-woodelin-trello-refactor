43bdd007b9d88f0c4800acc3901a14387a44974b
Corey Moore <monorthwest@gmail.com>
2019-06-27 08:27:13 -0700

    add state.js

---
+++ react-trello/src/State.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class State extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            deleteCard : null
+        }
+    }
+
+    handleClick = (event) => {
+        this.setState({
+            deleteCard : event.target.getAttribute('id')
+        }, () => console.log('finding id on card', this.state.deleteCard))
+    }
+
+    redner() {
+        const deleteItem = this.state.deleteCard
+    }
+
+
+}
