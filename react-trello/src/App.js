91c98500e27b33fdd8c9f2be326e3a56efc381c8
Corey Moore <monorthwest@gmail.com>
2019-06-27 09:43:12 -0700

    delete button working in console

--- react-trello/src/App.js
+++ react-trello/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import List from './List'
 import './App.css';
+import STORE from './STORE'
 
+// static defaultProps
 class App extends Component {
   static defaultProps = {
     store: {
@@ -10,8 +12,16 @@ class App extends Component {
     }
   };
 
+  state = {
+    store: STORE,
+  }
+
+  cardDelete = () => {
+    console.log('going to delete the card');
+  }
+
   render() {
-    const { store } = this.props
+    const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
@@ -23,6 +33,7 @@ class App extends Component {
               key={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              cardDelete={this.cardDelete}
             />
           ))}
         </div>
@@ -32,3 +43,13 @@ class App extends Component {
 }
 
 export default App;
+
+// deleteCard = (CardId, ListId) => { const aC = {...this.state.allCards} delete aC[CardId]; 
+
+// console.log(aC); 
+
+// this.setState({ lists:this.state.lists.map((list)=>{ 
+//   return { 
+//     id:list.id, header:list.header, cardIds:list.cardIds.filter((item)=>item!==CardId) 
+//   } }), allCards: aC,
+// }) };
--- react-trello/src/State.js
+++ react-trello/src/State.js
@@ -1,22 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
-class State extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            deleteCard : null
-        }
-    }
+// class State extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             deleteCard : null
+//         }
+//     }
 
-    handleClick = (event) => {
-        this.setState({
-            deleteCard : event.target.getAttribute('id')
-        }, () => console.log('finding id on card', this.state.deleteCard))
-    }
+//     handleClick = (event) => {
+//         this.setState({
+//             deleteCard : event.target.getAttribute('id')
+//         }, () => console.log('finding id on card', this.state.deleteCard))
+//     }
 
-    redner() {
-        const deleteItem = this.state.deleteCard
-    }
+//     redner() {
+//         const deleteItem = this.state.deleteCard
+//     }
 
-
-}
+// }
